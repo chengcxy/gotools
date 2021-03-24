8862e60cc742e0ce970d7ddf0943cbb4401fccb2
chengxinyao <xinyao.cheng@zerone.com.cn>
2021-03-25 00:02:28 +0800

    md5

--- utils/util.go
+++ utils/util.go
@@ -9,7 +9,17 @@ import (
 	"os"
 	"strings"
 	"path"
+    "crypto/md5"
+    "io"
 )
+
+func Md5(str string) string {
+    w := md5.New()
+    io.WriteString(w, str)
+    md5str := fmt.Sprintf("%x", w.Sum(nil))
+    return md5str
+}
+
 
 
 
